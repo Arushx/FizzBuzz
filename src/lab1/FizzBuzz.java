aad597780a83a6fede0ae6e22ad8468a8b5e046f
RushZ <96019968+Arushx@users.noreply.github.com>
2026-09-14 13:54:54 -0400

    Added main.java and completed tasks up to task 3.2

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,7 +6,7 @@ package lab1;
 
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main() {
 
 //        for (int i = 1; i <= 100; i++) {
 //
@@ -42,7 +42,7 @@ class FizzBuzz {
         }
     }
 
-    private static int DoFizzBuzz(int i) {
+    public static int DoFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,4 +1,8 @@
 package lab1;
 
 public class Main {
+    public static void main() {
+        FizzBuzz.DoFizzBuzz(5);
+        FizzBuzz.main();
+    }
 }
