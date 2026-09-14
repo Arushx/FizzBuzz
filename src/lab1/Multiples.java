18234fbb3ad7937daf5ed571e38e6df176c85063
RushZ <96019968+Arushx@users.noreply.github.com>
2026-09-14 14:35:54 -0400

    finished multiples section extra work

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,13 +3,25 @@ package lab1;
 public class Multiples {
     public static void main() {
 
-        System.out.println(multiples(1000,3,5));
+        System.out.println(multiples());
     }
 
     public static int multiples(int n, int a, int b) {
         int count = 0;
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i < n; i++) {
             boolean testMulti = i % a == 0 || i % b == 0;
+            if (testMulti) {
+                count = count + 1;
+            }
+        }
+
+        return count;
+    }
+
+    public static int multiples() {
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean testMulti = i % 3 == 0 || i % 5 == 0;
             if (testMulti) {
                 count = count + 1;
             }
